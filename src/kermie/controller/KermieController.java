a726cbaeeb09d2860dd4d6599d409d5242b8b01d
Xyrephon <jtuc1613@740s-pl-115.local>
2016-11-10 09:54:43 -0700

    Changed the controller in Kermie.

--- src/kermie/controller/KermieController.java
+++ src/kermie/controller/KermieController.java
@@ -37,7 +37,7 @@ public class KermieController
 		numbers = new int [] {1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1};
 	}
 	
-	public Kermie[] getHipsters()
+	public Kermie[] getKermie()
 	{
 		return kermie;
 	}
